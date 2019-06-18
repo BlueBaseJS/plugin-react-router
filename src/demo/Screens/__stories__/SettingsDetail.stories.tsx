8c4adad638f89e1831981d383a710ab671739483
Asad Razzaq <asad.razzaq@blueeast.com>
2019-06-18 16:15:27 +0500

    fix(MemoryRouter): stories added

--- src/demo/Screens/__stories__/SettingsDetail.stories.tsx
+++ src/demo/Screens/__stories__/SettingsDetail.stories.tsx
@@ -3,6 +3,7 @@
 import { BlueBaseApp } from '@bluebase/core';
 // import { MockedProvider } from 'react-apollo/test-utils';
 import Plugin from '../../../index';
+import PluginDemo from '../../index';
 import React from 'react';
 // import { SettingsDetailScreen } from '../SettingsDetail';
 import storiesOf from '@bluebase/storybook-addon';
@@ -13,17 +14,15 @@ storiesOf('Setting', module).add('Basic Example', () => (
 	<BlueBaseApp
 		plugins={[
 			Plugin,
+			PluginDemo,
 			require('@bluebase/plugin-apollo'),
-			// require('@mevris/client-plugin-ui'),
-			// require('@bluebase/plugin-react-router'),
 			require('@bluebase/plugin-material-ui'),
-			// require('@bluebase/plugin-json-schema-components'),
 		]}
-		// configs={{
-		// 	'plugin-react-router.routerProps': {
-		// 		location: { pathname: '/p/things/123/settings' },
-		// 	},
-		// 	'plugin-react-router.routerType': 'memory',
-		// }}
+		configs={{
+			'plugin-react-router.routerProps': {
+				initialEntries: [{ pathname: '/p/settings/foo?a=b' }],
+			},
+			'plugin-react-router.routerType': 'memory',
+		}}
 	/>
 ));
